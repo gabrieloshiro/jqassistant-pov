481682a8190fbc0848cbddf49c6e213c0469dac7
Gabriel Oshiro <gabrieloshiro@gmail.com>
2019-12-17 22:37:40 -0500

    initial commit

---
+++ src/main/java/com/example/jqassistantpov/JqassistantPovApplication.java
@@ -0,0 +1,13 @@
+package com.example.jqassistantpov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqassistantPovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JqassistantPovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jqassistantpov/JqassistantPovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jqassistantpov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqassistantPovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
